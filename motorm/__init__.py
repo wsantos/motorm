6478a32c35fa133a734acbc675da25cfe173aecf
Waldecir Santos <waldecir@gmail.com>
2014-08-16 21:38:50 -0300

    Fix a regression and save/update check.

--- motorm/__init__.py
+++ motorm/__init__.py
@@ -200,7 +200,7 @@ class AsyncModel(Model):
         self.validate()
 
         if self._initial == {} or (self._initial != {} and
-                                   "id" not in self._data):
+                                   self._data['id'] is None):
             _db[self.__collection__].save(
                 self.serialize(), callback=handle_save_response)
         else:
